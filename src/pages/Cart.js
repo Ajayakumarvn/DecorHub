e00c56d96c6a771752563b9478a9c89f33d6882b
Ajay <ajayakumaraji13@gmail.com>
2023-04-07 01:18:26 +0530

    changes

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -2,14 +2,16 @@ import React, { Fragment } from "react";
 import classes from "./Cart.module.css";
 import CartComponent from "./CartComponent";
 import { useStateValue } from "../store/StateProvider";
+import { Link } from "react-router-dom";
 
 const Cart = () => {
   const [{ cart }, dispatch] = useStateValue();
   return (
     <Fragment>
-      <h1 className={classes["checkout-heading"]}>Your Cart</h1>
-      {cart.map((item) => (
+      <h1 className={classes["checkout-heading"]}>Cart Items</h1>
+      {cart.map((item, index) => (
         <CartComponent
+          key={index}
           id={item.id}
           title={item.title}
           image={item.image}
@@ -18,6 +20,27 @@ const Cart = () => {
           color={item.color}
         />
       ))}
+      <div>
+        {cart.length > 0 ? (
+          <div>
+            {cart.map((item) => (
+              <div key={item.id}>
+                <p>{item.name}</p>
+                <p>{item.price}</p>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div style={{ textAlign: "center" }}>
+            <h2>"Oops ! Your Cart is Empty "</h2>
+            <div>
+              <Link to="/">
+                <button className={classes["back-btn"]}>Naviagate back</button>
+              </Link>
+            </div>
+          </div>
+        )}
+      </div>
     </Fragment>
   );
 };
